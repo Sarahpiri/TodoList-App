45b5804bda24a40938a432810d5ae3e0e1cc514c
Sara <sarahpiri992@gmail.com>
2025-04-29 16:40:46 +0200

    update html

---
+++ main.js
@@ -0,0 +1,17 @@
+const completedTasks = [
+  {
+    name: "task1",
+    description: "gjgvghvjhvh",
+    tag: "low",
+    completed: true,
+  },
+];
+
+const ongoingTasks = [
+  {
+    name: "task1",
+    description: "gjgvghvjhvh",
+    tag: "low",
+    completed: false,
+  },
+];
